5e9395093bc7d5163d41336c157027c2d917eb24
simplyatul <atulthosar@gmail.com>
2019-08-04 22:45:25 +0530

    - Added Spring Boot Structure created from https://start.spring.io/

---
+++ src/main/java/org/simplyatul/cassandrademo/CassandrademoApplication.java
@@ -0,0 +1,13 @@
+package org.simplyatul.cassandrademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandrademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandrademoApplication.class, args);
+	}
+
+}
